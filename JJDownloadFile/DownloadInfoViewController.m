d23110ea86e81439f76b3e0b22e2646fe982ea98
lzc <zuchuang.liu@melot.cn>
2020-12-23 17:35:25 +0800

    URLConnect 配合 FileHandle 下载大文件

--- JJDownloadFile/DownloadInfoViewController.m
+++ JJDownloadFile/DownloadInfoViewController.m
@@ -7,11 +7,13 @@
 
 #import "DownloadInfoViewController.h"
 
-@interface DownloadInfoViewController ()
+@interface DownloadInfoViewController () <NSURLConnectionDataDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIProgressView *progress;
 
+@property (nonatomic, assign) NSInteger totalLength, currentLenght;
+@property (nonatomic, strong) NSFileHandle *fileHandle;
 
 @end
 
@@ -25,7 +27,7 @@
 }
 
 - (IBAction)downloadAction:(id)sender {
-    [self downloadWithURLSession];
+    [self downloadBigFileWtihURLConnection];
 }
 
 - (IBAction)cancelAction:(id)sender {
@@ -80,8 +82,66 @@
 
 //MARK: - 大文件
 
+- (void)downloadBigFileWtihURLConnection {
+    NSURL *URL = [NSURL URLWithString:@"https://free-video.boxueio.com/ConstantAndVariable_Swift3-9781ed6f7bec16a5b48ea466496cfacd.mp4"];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    [NSURLConnection connectionWithRequest:request delegate:self];
+    
+    self.totalLength = 0;
+    self.currentLenght = 0;
+}
 
+//MARK: - NSURLConnectionDataDelegate
 
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    NSLog(@" %s", __func__);
+    
+    self.totalLength = response.expectedContentLength;
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *documentFilePath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *filePath = [documentFilePath stringByAppendingPathComponent:response.suggestedFilename];
+    
+    NSLog(@" filePath: %@", filePath);
+    
+    if (![fileManager fileExistsAtPath:filePath]) {
+        [fileManager createFileAtPath:filePath contents:nil attributes:nil];
+    }
+    
+    self.fileHandle = [NSFileHandle fileHandleForWritingAtPath:filePath];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    // 如果使用 NSMutableData 拼接 data。可能会使内存过大导致 OOM。
+    
+    // 指定数据的写入位置 -- 文件内容的最后面
+    [self.fileHandle seekToEndOfFile];
+    // 向沙盒写入数据
+    [self.fileHandle writeData:data];
+    
+    self.currentLenght += data.length;
+    
+    self.progress.progress = self.currentLenght * 1.0 / self.totalLength;
+    NSLog(@" progress: %f", self.progress.progress);
+}
+
+- (void)connection:(NSURLConnection *)connection
+   didSendBodyData:(NSInteger)bytesWritten
+ totalBytesWritten:(NSInteger)totalBytesWritten
+totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite {
+    NSLog(@" %s", __func__);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSLog(@" %s", __func__);
+    
+    [self.fileHandle closeFile];
+    self.fileHandle = nil;
+    
+    self.currentLenght = 0;
+    self.totalLength = 0;
+}
 
 
 
